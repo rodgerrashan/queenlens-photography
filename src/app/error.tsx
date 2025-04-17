17df705c736bd0182cc96d0e9d6a4b1238e4cff5
rodgerrashan <rodrasjay@gmail.com>
2025-04-17 07:08:57 +0530

    404 added and root modified

---
+++ src/app/error.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import Image from 'next/image';
+
+export default function Error({ reset }: { reset: () => void }) {
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen text-center px-4">
+      <div className="p-4">
+        <Image
+          src="https://www.queenlens.lk/images/logo/smLogo.png"
+          alt="Logo"
+          width={128}
+          height={128}
+          className="animate-pulse"
+        />
+      </div>
+
+      <h2 className="text-2xl font-semibold mt-4 text-red-600">Something went wrong!</h2>
+      <p className="mt-2 text-gray-600">Unexpected error occurred.</p>
+      <button
+        onClick={reset}
+        className="mt-4 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
+      >
+        Try Again
+      </button>
+    </div>
+  );
+}
