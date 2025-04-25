b520ab8fc068a21e342e4eaae6d0aa4e6a7d261b
rodgerrashan <rodrasjay@gmail.com>
2025-04-25 22:46:46 +0530

    refactor: enhance error component to display error message and update button styles

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -2,7 +2,13 @@
 
 import Image from 'next/image';
 
-export default function Error({ reset }: { reset: () => void }) {
+export default function Error({ 
+  error,
+  reset 
+}: { 
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
   return (
     <div className="flex flex-col justify-center items-center min-h-screen text-center px-4">
       <div className="p-4">
@@ -17,9 +23,10 @@ export default function Error({ reset }: { reset: () => void }) {
 
       <h2 className="text-2xl font-semibold mt-4 text-red-600">Something went wrong!</h2>
       <p className="mt-2 text-gray-600">Unexpected error occurred.</p>
+      <p className="mt-2 text-sm text-gray-500">{error.message}</p>
       <button
         onClick={reset}
-        className="mt-4 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
+        className="mt-4 px-4 py-2 bg-gray-900 text-white rounded hover:bg-gray-950 transition"
       >
         Try Again
       </button>
