243df8a927c7f77b3b1d533e943f70ad31caba59
RodgerJay <124046523+rodgerrashan@users.noreply.github.com>
2026-01-17 22:32:48 +0530

    Update copyright year from 2025 to 2026

--- src/components/Copyrights.tsx
+++ src/components/Copyrights.tsx
@@ -1,7 +1,7 @@
 export default function copyrights() {
     return(
         <div className="text-center text-xs text-slate-100 bg-gray-950 px-4 py-1">
-            <p>&copy; 2025 Queenlens Photography. All rights reserved.</p>
+            <p>&copy; 2026 Queenlens Photography. All rights reserved.</p>
         </div>
     );
 }
